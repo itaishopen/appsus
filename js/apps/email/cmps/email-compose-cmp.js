e71fbefd6a9958bff608cf7b0409179321d1942f
itaishopen <itaishopen@gmail.com>
2019-02-27 08:30:41 +0200

    organize

--- js/apps/email/cmps/email-compose-cmp.js
+++ js/apps/email/cmps/email-compose-cmp.js
@@ -1,58 +1,31 @@
 import { eventBus, EVENT_FEEDBACK } from '../event-bus.js'
 
 export default {
+    props: [''],
     template:`
-    <section v-if="showMsg" class="user-msg flex justify-center align-center">
-        <div :class="msgType" >
-            <div class="flex">
-                <button :style="{color:btnColor}"  @click="closeMsg" class="fas fa-times-circle"></button>
-                <i :class="iconClass"></i>
-                <div>
-                    <p>{{msg.txt}}</p>
-                    <router-link class="msg-link" v-if="msg.link !== ''" :to='msg.link' @click="closeMsg">Go to book</router-link>
-                </div>
-            </div>
-        </div>
+    <section class="email-compose">
         
     </section>
     `,
      data() {
         return {
-            showMsg: false,
-            msg: '',
-            msgType:'',  
+              
         }
     },
     created() {
-        eventBus.$on(EVENT_FEEDBACK,(msg,type) => {
-            this.showMsg = true;
-            this.msg = msg;
-            this.msgType = type;
-            
-        })
+        
     },
-    // mounted() {
-    //     document.querySelector('.msg-link').onclick = this.closeMsg;
-    //   },
+    mounted() {
+        
+      },
     watch: {
-        "document.querySelector('.msg-link').classList": function (classList) {
-            this.showMsg = false;
-        }
+        
     },
     methods:{
-        closeMsg() {
-            this.showMsg = false;
-        }
+        
     },
    
     computed :{
-        iconClass(){
-            return this.msgType === 'success' ?'fas fa-check':'fas fa-times';
-        },
-        btnColor(){
-            return this.msgType === 'success' ?'green':'red';  
-        }
+        
     }
-
-    
 }

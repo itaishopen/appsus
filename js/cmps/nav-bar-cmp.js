5d6c79a07b3895363ea99f84be82e3cf7c04eb8a
Yanai Avnet <bzakabz@gmail.com>
2019-02-28 20:19:06 +0200

    2nd day final

--- js/cmps/nav-bar-cmp.js
+++ js/cmps/nav-bar-cmp.js
@@ -1,8 +1,8 @@
 export default {
     template: `
     <section class="nav" id="nav-bar">
-        <router-link exact to="/">Home</router-link>
-        <router-link exact to="/miss-keep">Miss Keep</router-link>
+        <router-link exact to="/">Home</router-link> |
+        <router-link exact to="/miss-keep">Miss Keep</router-link> |
         <router-link exact to="/email">Mr Email</router-link>
     </section>
     `
--- js/pages/home-page-cmp.js
+++ js/pages/home-page-cmp.js
@@ -4,9 +4,8 @@ export default {
     <section class="home-page">
         <div class="flex column">
             <div class="home-header flex align-center">
-                <h1>welcome to miss books in a single page view</h1>
+                <h1>welcome to <span class="logo-txt">AppSus</span></h1>
             </div>
-            <!-- <img src="img/miss-books.jpg" alt="Home Page"> -->
         </div>
     </section>
     `,

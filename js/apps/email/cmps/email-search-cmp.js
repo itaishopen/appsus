0ec64b75b3bb9c1e1cd1dd0ec99feef9cfe13541
itaishopen <itaishopen@gmail.com>
2019-03-03 20:56:01 +0200

    emails

--- js/apps/email/cmps/email-search-cmp.js
+++ js/apps/email/cmps/email-search-cmp.js
@@ -4,7 +4,7 @@ export default {
         <div class="search-tab">
             <input type="search"  v-model="inputSearch" placeholder="Search your emails" title="Type in a search parameter" @input="onSearch"/>
         </div>
-        <div class="search-checkbox flex">
+        <div class="search-checkbox flex" title="Search in all emails">
             <input type="checkbox" id="all" value="all" v-model="selected">
             <label for="all">Search in all emails</label>
         </div> 
